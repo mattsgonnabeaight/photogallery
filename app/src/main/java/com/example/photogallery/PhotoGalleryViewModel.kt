6e8c8ebfb3797a7e5fe8843fb0cf93ba93ecdeb7
funnyyellowink <88548679+funnyyellowink@users.noreply.github.com>
2021-11-02 10:35:11 +0300

    initial commit

---
+++ app/src/main/java/com/example/photogallery/PhotoGalleryViewModel.kt
@@ -0,0 +1,32 @@
+package com.example.photogallery
+
+import android.app.Application
+import com.example.photogallery.api.FlickrFetchr
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+
+class PhotoGalleryViewModel(private val app: Application) : AndroidViewModel(app) {
+    val galleryItemLiveData: LiveData<List<GalleryItem>>
+
+    private val flickrFetchr = FlickrFetchr()
+    private val mutableSearchTerm = MutableLiveData<String>()
+    val searchTerm: String
+        get() = mutableSearchTerm.value ?: ""
+
+    init {
+        mutableSearchTerm.value = QueryPreferences.getStoredQuery(app)
+        galleryItemLiveData = Transformations.switchMap(mutableSearchTerm) { searchTerm ->
+            if (searchTerm.isBlank()) {
+                flickrFetchr.fetchPhotos()
+            } else {
+                flickrFetchr.searchPhotos(searchTerm)
+            }
+        }
+    }
+    fun fetchPhotos(query: String = "") {
+        QueryPreferences.setStoredQuery(app, query)
+        mutableSearchTerm.value = query
+    }
+}
